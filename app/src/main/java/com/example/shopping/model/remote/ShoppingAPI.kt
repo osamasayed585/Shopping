9d1107cbba1b82dd2355a831646d1524a1a291ef
ali <ali@elmeyasoft.com>
2021-10-24 09:44:43 +0200

    Add BottomNavigationView With - HomeFragment - ShoppingFragment

--- app/src/main/java/com/example/shopping/model/remote/ShoppingAPI.kt
+++ app/src/main/java/com/example/shopping/model/remote/ShoppingAPI.kt
@@ -1,7 +1,5 @@
 package com.example.shopping.model.remote
 
-import com.example.shopping.model.data.Data
-import com.example.shopping.model.data.User
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -9,16 +7,6 @@ import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ShoppingAPI {
-    @GET("profile")
-    suspend fun getProfile(@Header("Authorization") authorization: String): Response<User>
 
-    @POST("register")
-    suspend fun postRegister(@Body data: Data): Response<User>
-
-    @POST("login")
-    suspend fun postLogin(@Body data: Data): Response<User>
-
-    @POST("logout")
-    suspend fun postLogout(@Header("Authorization") authorization: String): Response<User>
 
 }
--- app/src/main/java/com/example/shopping/ui/MainActivity.kt
+++ app/src/main/java/com/example/shopping/ui/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.shopping.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.shopping.R
+import com.example.shopping.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navView: BottomNavigationView = binding.contentMain.navView
+        actionBar?.setDisplayHomeAsUpEnabled(true);
+        actionBar?.setHomeButtonEnabled(true);
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main2)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_shopping_cart
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
