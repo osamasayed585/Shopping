71e658e472bad0f91b9d5422640bc28b9eba9ba8
Osama Sayed <osamasayed151@gmail.com>
2021-10-25 12:49:17 +0200

    add AccountFragment.kt and

--- app/src/main/java/com/example/shopping/ui/dashboard/account/AccountFragment.kt
+++ app/src/main/java/com/example/shopping/ui/dashboard/account/AccountFragment.kt
@@ -1,10 +1,11 @@
-package com.example.shopping
+package com.example.shopping.ui.dashboard.account
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.shopping.databinding.FragmentAccountBinding
 import com.example.shopping.databinding.FragmentSplashBinding
 
@@ -22,7 +23,10 @@ class AccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.profileLogout.setOnClickListener {
+            // toDo I'll implementation this a code next time
+            Toast.makeText(context, "I'll implementation this a code next time", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
