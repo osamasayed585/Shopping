27eae64c13a569919bc1271d541072bf364e6cd5
Osama Sayed <osamasayed151@gmail.com>
2021-10-28 02:53:35 +0200

    Update splash

--- app/src/main/java/com/example/shopping/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/shopping/ui/splash/SplashFragment.kt
@@ -53,51 +53,33 @@ class SplashFragment : Fragment() {
 
 
     private fun toWhere() {
-
-        initButtons()
-
-        val sharedPreferences = requireActivity().applicationContext.getSharedPreferences(
-            SHOPPING_DATA,
-            Context.MODE_PRIVATE
-        )
+        val sharedPreferences = requireActivity().applicationContext.getSharedPreferences(SHOPPING_DATA, Context.MODE_PRIVATE)
         val statusLogin = sharedPreferences.getBoolean("STATUS_LOGIN", false)
         val statusRegister = sharedPreferences.getBoolean("STATUS_REGISTER", false)
-        if (statusLogin || statusRegister) {
-            Handler(Looper.getMainLooper()).postDelayed({
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (statusLogin || statusRegister) {
                 Static.onLogin?.onDone()
-            }, 2000)
+            } else {
 
-            return
-        }
-
-        GlobalScope.launch {
-            delay(100)
-            GlobalScope.launch(Dispatchers.Main) {
-                binding.btLogin  .scaleUp(1500)
-                binding.btSignUp .scaleUp(1500)
-                binding.skipLogin.scaleUp(1500)
-
+                GlobalScope.launch {
+                    delay(100)
+                    GlobalScope.launch(Dispatchers.Main) {
+                        binding.btLogin.scaleUp(1500)
+                        binding.btSignUp.scaleUp(1500)
+                        binding.skipLogin.scaleUp(1500)
+                    }
+                }
             }
-        }
-    }
+        }, 3000)
 
-<<<<<<< HEAD
+    }
     private fun initButtons(state: Boolean) {
         if (state) {
             binding.skipLogin.visibility = View.VISIBLE
             binding.btLogin.visibility = View.VISIBLE
             binding.btSignUp.visibility = View.VISIBLE
-        }else{
-            binding.skipLogin.visibility = View.GONE
-            binding.btLogin.visibility = View.GONE
-            binding.btSignUp.visibility = View.GONE
         }
-=======
-    private fun initButtons() {
-        binding.btLogin  .isVisible=false
-        binding.btSignUp .isVisible=false
-        binding.skipLogin.isVisible=false
->>>>>>> 9f220f977ad08fc144b7f4623ec12c0f263565e3
     }
 
     private fun initStatusBar(state: Boolean) {
